5a9ec82ccf13c57c8ad5d24c9f0f3a0af8fb0339
Vinícius <vkdaros@ime.usp.br>
2014-10-21 10:33:26 -0200

    Fixing indentation and style of first generated code. Removing HaxeFlixel splash.

--- prototypes/haxeflixel/factory0/source/Main.hx
+++ prototypes/haxeflixel/factory0/source/Main.hx
@@ -8,61 +8,65 @@ import flash.Lib;
 import flixel.FlxGame;
 import flixel.FlxState;
 
-class Main extends Sprite 
-{
-	var gameWidth:Int = 640; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 480; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = MenuState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	
-	// You can pretty much ignore everything from here on - your code should go in your states.
-	
-	public static function main():Void
-	{	
-		Lib.current.addChild(new Main());
-	}
-	
-	public function new() 
-	{
-		super();
-		
-		if (stage != null) 
-		{
-			init();
-		}
-		else 
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
-	}
-	
-	private function init(?E:Event):Void 
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-		
-		setupGame();
-	}
-	
-	private function setupGame():Void
-	{
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
+class Main extends Sprite {
+    // Width of the game in pixels (might be less / more in actual pixels
+    // depending on your zoom).
+    var gameWidth:Int = 640;
 
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
+    // Height of the game in pixels (might be less / more in actual pixels
+    // depending on your zoom).
+    var gameHeight:Int = 480;
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
-	}
+    // The FlxState the game starts with.
+    var initialState:Class<FlxState> = MenuState;
+
+    // If -1, zoom is automatically calculated to fit the window dimensions.
+    var zoom:Float = -1;
+
+    // How many frames per second the game should run at.
+    var framerate:Int = 60;
+
+    // Whether to skip the flixel splash screen that appears in release mode.
+    var skipSplash:Bool = true;
+
+    // Whether to start the game in fullscreen on desktop targets
+    var startFullscreen:Bool = false;
+
+    public static function main():Void {
+        Lib.current.addChild(new Main());
+    }
+
+    public function new() {
+        super();
+
+        if (stage != null) {
+            init();
+        }
+        else {
+            addEventListener(Event.ADDED_TO_STAGE, init);
+        }
+    }
+
+    private function init(?E:Event):Void {
+        if (hasEventListener(Event.ADDED_TO_STAGE)) {
+            removeEventListener(Event.ADDED_TO_STAGE, init);
+        }
+
+        setupGame();
+    }
+
+    private function setupGame():Void {
+        var stageWidth:Int = Lib.current.stage.stageWidth;
+        var stageHeight:Int = Lib.current.stage.stageHeight;
+
+        if (zoom == -1) {
+            var ratioX:Float = stageWidth / gameWidth;
+            var ratioY:Float = stageHeight / gameHeight;
+            zoom = Math.min(ratioX, ratioY);
+            gameWidth = Math.ceil(stageWidth / zoom);
+            gameHeight = Math.ceil(stageHeight / zoom);
+        }
+
+        addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+    }
 }
