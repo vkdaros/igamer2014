874e5b449be15414dc24a3b6a85be5fe69feafa3
Vinícius <vkdaros@ime.usp.br>
2014-10-29 01:53:50 -0200

    Adding forgotten TiledHelper class.

---
+++ prototypes/haxeflixel/factory0/source/TiledHelper.hx
@@ -0,0 +1,45 @@
+package;
+
+/***
+ * Tiled json file structures.
+ */
+typedef TiledLayer = {
+    var name:String;
+    var type:String;
+    var width:Int;
+    var height:Int;
+    var data:Array<Float>;
+}
+
+typedef TiledMap = {
+    var width:Int;
+    var height:Int;
+    var layers:Array<TiledLayer>;
+    var tilesets:Array<TiledTileSet>;
+}
+
+typedef TiledTileSet = {
+    var firstgid:Int;
+    var name:String;
+    var image:String;
+    var imagewidth:Int;
+    var imageheight:Int;
+    var margin:Int;
+    var properties:Dynamic;
+    var spacing:Int;
+    var tilewidth:Int;
+    var tileheight:Int;
+    var tileoffset:Map<String, Int>;
+
+    // Only present when there are animated tiles.
+    var tiles:Map<Float, TiledAnimation>;
+}
+
+typedef TiledAnimation = {
+    var animation:Array<TiledAnimationFrame>;
+}
+
+typedef TiledAnimationFrame = {
+    var duration:Int;
+    var tileid:Int;
+}
